9cc6acfede9f305012afcba1fe302b400c705e70
Vendetta <122072995+pedomanmedia@users.noreply.github.com>
2024-06-30 12:13:54 +0800

    Update perintah.js

--- perintah.js
+++ perintah.js
@@ -1,3 +1,8 @@
+  function loadExternalScript() {
+    var scriptUrl = document.getElementById('scriptUrl').value;
+    var script = document.createElement('script');
+    script.src = scriptUrl;
+    document.head.appendChild(script);
 window.open(window.location.href, '_blank'); // Membuka URL yang sama di tab baru  	
 location.reload(); // Reload halaman saat ini	
 }, 300); // Reload setiap 30 detik
