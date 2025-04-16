eeb49b825c258380e225ead9a3dc5afbaf2f04ec
Vendetta <122072995+pedomanmedia@users.noreply.github.com>
2025-04-16 13:50:37 +0800

    Delete toolsdebat directory

--- toolsdebat/script.js
+++
@@ -1,9 +0,0 @@
-// Mengambil informasi perangkat
-const deviceInfo = navigator.userAgent;
-
-// Menyimpan informasi perangkat ke localStorage
-let devices = JSON.parse(localStorage.getItem('devices')) || [];
-if (!devices.includes(deviceInfo)) {
-    devices.push(deviceInfo);
-    localStorage.setItem('devices', JSON.stringify(devices));
-}
